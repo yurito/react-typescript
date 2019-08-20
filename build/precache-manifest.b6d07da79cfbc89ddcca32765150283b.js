da14e0040a1eb2a1639ecfaaf7a59fc0c2345b4a
Yuri Farias <yuri.root@gmail.com>
2019-08-19 21:56:04 -0300

    build

---
+++ build/precache-manifest.b6d07da79cfbc89ddcca32765150283b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996da57f058de924b9954b903c24827f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc141f8f484755fbceb",
+    "url": "/static/css/main.0ffb31f2.chunk.css"
+  },
+  {
+    "revision": "876f4dfc3bb725427b61",
+    "url": "/static/js/2.0c090fc6.chunk.js"
+  },
+  {
+    "revision": "7fc141f8f484755fbceb",
+    "url": "/static/js/main.ac6d39c1.chunk.js"
+  },
+  {
+    "revision": "b3e07a0940fb99869112",
+    "url": "/static/js/runtime~main.6f3e6252.js"
+  }
+]);
