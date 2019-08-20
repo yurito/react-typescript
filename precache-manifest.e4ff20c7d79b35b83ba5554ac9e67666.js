3e3fc249524b15e89a5740905cf7621093bae994
Yuri Farias <yuri.root@gmail.com>
2019-08-20 11:13:18 -0300

    Updates

---
+++ precache-manifest.e4ff20c7d79b35b83ba5554ac9e67666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b273baa1c1685ad22d896839af16c1c3",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "0b6fa190f747ad6d415a",
+    "url": "/site/static/css/main.ba67fc3b.chunk.css"
+  },
+  {
+    "revision": "23f9801343bd5a01ea5f",
+    "url": "/site/static/js/2.ee16a285.chunk.js"
+  },
+  {
+    "revision": "0b6fa190f747ad6d415a",
+    "url": "/site/static/js/main.a3cc4e29.chunk.js"
+  },
+  {
+    "revision": "ec12fd4ce714c34b8ad9",
+    "url": "/site/static/js/runtime~main.86b6ef6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/site/precache-manifest.2a02d11908e7a9ba5777832cc1238c0b.js"
+  "/site/precache-manifest.e4ff20c7d79b35b83ba5554ac9e67666.js"
 );
 
 self.addEventListener('message', (event) => {
